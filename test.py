9f6a061c39a3b026ef4c929461e13e61e24f8348
zothor <kevinzothor1@hotmail.com>
2015-11-02 11:48:46 +0100

    Blablabla

--- test.py
+++ test.py
@@ -2,6 +2,6 @@ __author__ = 'kevin'
 
 from tkinter import *
 root = Tk()
-button_1 = Label(root, text= " het werkt!" )
+button_1 = Label(root, text= " bram is homo!" )
 button_1.pack()
 root.mainloop()
