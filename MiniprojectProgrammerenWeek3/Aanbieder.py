cc064ea22b8fc7a33b54df5c8cb7d7a6aeefa20a
Vallkyr <ricardo.bouwman@outlook.com>
2015-11-04 11:21:50 +0100

    Aanbieder bepalen

--- MiniprojectProgrammerenWeek3/Aanbieder.py
+++ MiniprojectProgrammerenWeek3/Aanbieder.py
@@ -1,4 +1,16 @@
 __author__ = 'jouke-bouwe'
 
-from  MiniprojectProgrammerenWeek3 import  Klant
+from  MiniprojectProgrammerenWeek3 import FilmsOphalen
 
+FilmsOphalen.getJaartal()
+
+if jaar <= 1969:
+    aanbieder = "Kevin"
+elif jaar >= 1970 <= 1989:
+    aanbieder = "Frank"
+elif jaar >= 1990 <= 1999:
+    aanbieder = "Bram"
+elif jaar >= 2000 <= 2009:
+    aanbieder = "Jouke-Bouwe"
+elif jaar >= 2010:
+    aanbieder = "Ricardo"
