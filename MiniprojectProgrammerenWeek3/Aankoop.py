860c8d7319d8b3580e3d5e398c37fed985c77ca0
Vallkyr <ricardo.bouwman@outlook.com>
2015-11-04 14:28:12 +0100

    Aankopen en stuff

--- MiniprojectProgrammerenWeek3/Aankoop.py
+++ MiniprojectProgrammerenWeek3/Aankoop.py
@@ -1,20 +1,27 @@
 __author__ = 'Rica'
 
+import random
+from MiniprojectProgrammerenWeek3 import Klant
 from  MiniprojectProgrammerenWeek3 import FilmsOphalen
 
-i=0
-
+uniekeCode = ""
 alleLijsten_van_de_films = []
 
 #Maakt lijsten van de films van die dag
+i=0
 while i < len(FilmsOphalen.titelFilms):
     nieuwelijst = []
     alleLijsten_van_de_films.append(nieuwelijst)
     i += 1
 
-print (alleLijsten_van_de_films)
+def genereerCode():
+    global uniekeCode
+    uniekeCode = random.randrange(1000,9999)
+
+genereerCode()
+FilmsOphalen.GetStart()
 
 #Aanschaf knop, voegt gebruikersnaam toe aan ID van de film
-alleLijsten_van_de_films[0].append("asdaweasd")
+alleLijsten_van_de_films[0].append(FilmsOphalen.starttijd + " " + Klant.huidigAchternaam + " " + str(uniekeCode))
 
-print(alleLijsten_van_de_films[0])
+print(sorted(alleLijsten_van_de_films[0]))
