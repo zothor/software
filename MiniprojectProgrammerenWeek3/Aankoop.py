a60eb21013f9c0cd53b62d69e902fceefaf5cccc
Vallkyr <ricardo.bouwman@outlook.com>
2015-11-04 15:02:05 +0100

    Aanbieders en aankopen opslaan

--- MiniprojectProgrammerenWeek3/Aankoop.py
+++ MiniprojectProgrammerenWeek3/Aankoop.py
@@ -1,11 +1,53 @@
 __author__ = 'Rica'
 
+import csv
 import random
 from MiniprojectProgrammerenWeek3 import Klant
 from  MiniprojectProgrammerenWeek3 import FilmsOphalen
 
+csvFrankBestand = 'C:/Users/Rica/PycharmProjects/software/MiniprojectProgrammerenWeek3/frankBestand.csv'
+csvKevinBestand = 'C:/Users/Rica/PycharmProjects/software/MiniprojectProgrammerenWeek3/kevinBestand.csv'
+csvBramBestand = 'C:/Users/Rica/PycharmProjects/software/MiniprojectProgrammerenWeek3/bramBestand.csv'
+csvJoukeBestand = 'C:/Users/Rica/PycharmProjects/software/MiniprojectProgrammerenWeek3/joukeBestand.csv'
+csvRicardoBestand = 'C:/Users/Rica/PycharmProjects/software/MiniprojectProgrammerenWeek3/ricardoBestand.csv'
+
+fieldnames = ["naam", "achternaam", "film"]
+
 uniekeCode = ""
 alleLijsten_van_de_films = []
+
+def maakFrankHeaders():
+    frankBestand = open(csvFrankBestand, 'a', newline='')
+    writer = csv.writer(frankBestand,dialect='excel')
+    writer.writerow(fieldnames)
+
+def maakKevinHeaders():
+    kevinBestand = open(csvKevinBestand, 'a', newline='')
+    writer = csv.writer(kevinBestand,dialect='excel')
+    writer.writerow(fieldnames)
+
+def maakBramHeaders():
+    bramBestand = open(csvBramBestand, 'a', newline='')
+    writer = csv.writer(bramBestand,dialect='excel')
+    writer.writerow(fieldnames)
+
+def maakJoukeHeaders():
+    joukeBestand = open(csvJoukeBestand, 'a', newline='')
+    writer = csv.writer(joukeBestand,dialect='excel')
+    writer.writerow(fieldnames)
+
+def maakRicardoHeaders():
+    ricardoBestand = open(csvRicardoBestand, 'a', newline='')
+    writer = csv.writer(ricardoBestand,dialect='excel')
+    writer.writerow(fieldnames)
+
+def voegFrankKlantToe():    #Hebben nog input nodig welke film wordt geselecteerd
+    try:
+        frankBestand = open(csvFrankBestand, 'a', newline='')
+        writer = csv.DictWriter(frankBestand,delimiter=',', fieldnames=fieldnames)
+        writer.writerow({"naam": Klant.huidigNaam, "achternaam": Klant.huidigAchternaam, "film":input})
+    finally:
+        frankBestand.close()
 
 #Maakt lijsten van de films van die dag
 i=0
